9fcb42bf7a8e12988f1dba6542ee1bc0741e0470
liuliang <159008390@qq.com>
2019-05-25 19:20:54 +0800

    setData

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import store from './stores'
 import axios from 'axios'
 Vue.prototype.axios = axios
 Vue.config.productionTip = false
+Vue.filter('setWH', (url, arg) => {
+  return url.replace(/w\.h/, arg)
+})
 
 new Vue({
   router,
